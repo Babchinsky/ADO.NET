63c43c4efcef12888d2c1e72ff1bf96d8072b49a
Babchinsky <113105752+Babchinsky@users.noreply.github.com>
2024-02-19 05:28:50 +0200

    Add files via upload

---
+++ Homeworks/14.02.2024 StationeryCompany/14.02.2024 StationeryCompany/14.02.2024 StationeryCompany/View/SalesManagerSelectionForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _14._02._2024_StationeryCompany.View
+{
+    public partial class SalesManagerSelectionForm : Form
+    {
+        // В этом событии будет передаваться выбранный менеджер по продажам
+        public event EventHandler<string> ManagerSelected;
+
+        public SalesManagerSelectionForm(List<string> salesManagers)
+        {
+            InitializeComponent();
+
+            // Заполняем ComboBox менеджерами по продажам
+            comboBoxSalesManagers.DataSource = salesManagers;
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            // Передаем выбранного менеджера в основную форму
+            ManagerSelected?.Invoke(this, comboBoxSalesManagers.SelectedItem?.ToString());
+            Close();
+        }
+    }
+}
